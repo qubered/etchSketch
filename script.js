aea993ecb3843e133e34355ee90aec589edea99a
Jayden Nawotka <156985453+qubered@users.noreply.github.com>
2025-01-22 01:38:13 +1100

    Add logic to change amount of pixels

--- script.js
+++ script.js
@@ -18,23 +18,42 @@ function gridDraw(pixelCount) {
 
 gridDraw(16)
 
+const sizeSubmit = document.querySelector("#sizeSubmit");
+const sizeBox = document.querySelector("#sizeBox");
 
+sizeSubmit.addEventListener("click", () => {
+    if (isNaN(sizeBox.value) ||sizeBox.value > 64 || sizeBox.value < 2) {
+        alert("Enter a number between 2 and 64")
+        focus(sizeBox)
+    }
+    else {
+        sizeBox.placeholder = sizeBox.value;
+        parentDiv.replaceChildren();
+        gridDraw(sizeBox.value);
+        const grids = document.querySelectorAll("#grid");
+        gridListener()
+    }
 
-
-const grids = document.querySelectorAll("#grid");
-
-grids.forEach((grid) => {
-    let opacity = 0.1;
-    
-    grid.addEventListener("mouseover", () => {
-        opacity = Math.min(opacity + 0.1, 1.0);
-        grid.style.background = `rgba(129, 7, 7, ${opacity})`;
-    })
 })
 
+function gridListener() {
+    const grids = document.querySelectorAll("#grid");
+    grids.forEach((grid) => {
+        let opacity = 0.1;
+        
+        grid.addEventListener("mouseover", () => {
+            opacity = Math.min(opacity + 0.1, 1.0);
+            grid.style.background = `rgba(129, 7, 7, ${opacity})`;
+        })
+    })
+}
+
 clearButton.addEventListener("click", () => {
+    const grids = document.querySelectorAll("#grid");
     grids.forEach((grid) => {
         grid.style.background = "rgba(129, 7, 7, 0)";
     })
 })
 
+sizeBox.placeholder = pixelCount;
+gridListener()
