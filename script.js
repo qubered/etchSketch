da8110303542e6f48503736e74e41086d1ac7f3c
Jayden Nawotka <156985453+qubered@users.noreply.github.com>
2025-01-22 01:15:55 +1100

    Add some CSS formatting and a reset button

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const parentDiv = document.querySelector(".parentDiv");
 
+const clearButton = document.querySelector("#clear");
+
 for (let i=0; i < 16; i++) {
     const horizontalDiv = document.createElement("div");
     horizontalDiv.id = "horizontalDiv";
@@ -18,7 +20,13 @@ grids.forEach((grid) => {
     let opacity = 0.1;
     
     grid.addEventListener("mouseover", () => {
-        opacity = Math.min(opacity + 0.2, 1.0);
+        opacity = Math.min(opacity + 0.1, 1.0);
         grid.style.background = `rgba(129, 7, 7, ${opacity})`;
+    })
+})
+
+clearButton.addEventListener("click", () => {
+    grids.forEach((grid) => {
+        grid.style.background = "rgba(129, 7, 7, 0)";
     })
 })
